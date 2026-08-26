2bb9c017a0a07341e2d980757f406a22dbd11c98
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-26 19:45:39 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,16 @@
 from setuptools import setup, Extension
-import os
 
-# Define the C extension module and link SDL2
-# (Note: On Linux/Termux you link against -lSDL2, on Windows/macOS you link appropriately)
 c_extension = Extension(
-    'pyda._core',
-    sources=['pyda/core.c'],
+    'pyda.pygame._core',
+    sources=['pyda/pygame/core.c'],
     libraries=['SDL2'],
-    extra_compile_args=['-O3'],  # Maximum optimization for raw speed
+    extra_compile_args=['-O3'],
 )
 
 setup(
     name="pyda",
     version="1.0.0",
-    description="A high-performance C+Python game engine built from scratch",
-    author="Sampath",
-    packages=['pyda'],
+    packages=['pyda', 'pyda.pygame', 'pyda.dangame'],
     ext_modules=[c_extension],
     zip_safe=False,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: C",
-        "Topic :: Games/Entertainment",
-    ],
 )
