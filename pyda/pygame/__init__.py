e221d07f48015bbd9551e1b58b403303b5533d4e
jangalacharansaisri963-eng <jangalacharansaisri963@gmail.com>
2026-08-26 19:52:54 +0530

    Update __init__.py

--- pyda/pygame/__init__.py
+++ pyda/pygame/__init__.py
@@ -1,37 +1,16 @@
 """
-pyda.pygame: Standalone native SDL2-powered game module.
+pyda.pygame: Full native rewrite of Pygame built on SDL2.
 """
+from ._core import init, quit, get_ticks, delay
+from .rect import Rect
+from .surface import Surface
+from .display import DisplayModule, display
+from .event import EventModule, event, QUIT, KEYDOWN, KEYUP, MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION
+from .time import Clock
+from . import draw
+from . import key
 
-from ._core import (
-    init,
-    quit,
-    set_mode,
-    flip,
-)
-
-class Rect:
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def colliderect(self, other):
-        return (
-            self.x < other.x + other.width and
-            self.x + self.width > other.x and
-            self.y < other.y + other.height and
-            self.y + self.height > other.y
-        )
-
-    def __repr__(self):
-        return f"<Rect(x={self.x}, y={self.y}, w={self.width}, h={self.height})>"
-
-class DisplayModule:
-    def set_mode(self, resolution):
-        return set_mode(resolution)
-    
-    def flip(self):
-        return flip()
-
-display = DisplayModule()
+__all__ = [
+    "init", "quit", "Rect", "Surface", "display", "event", "Clock", "draw", "key",
+    "QUIT", "KEYDOWN", "KEYUP", "MOUSEBUTTONDOWN", "MOUSEBUTTONUP", "MOUSEMOTION"
+]
